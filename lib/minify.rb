3319c03ecc56580f7c92442088c0a96e32205ee8
Christopher Giroir <kelsin@valefor.com>
2011-03-17 23:15:19 -0400

    Helpers fleshed out a bit

--- lib/minify.rb
+++ lib/minify.rb
@@ -1,19 +1,60 @@
+# Rails helpers
 if defined?(Rails)
   require 'minify/helper'
 end
 
+# YUI Compressor
 begin
   require 'yui/compressor'
 rescue LoadError => e
   # Failure is ok, won't compress files in this case, only concat them
 end
 
+# Other normal requires
+require 'yaml'
+
 module Minify
+  ROOT = File.expand_path((defined?(Rails) && Rails.root.to_s.length > 0) ? Rails.root : '.')
+  ASSET_FILE = File.join(ROOT, 'config', 'assets.yml')
+  JAVASCRIPT_DIR = File.join(ROOT, 'public', 'javascripts')
+  STYLESHEET_DIR = File.join(ROOT, 'public', 'stylesheets')
+
   def self.lessc_available?
     system("which -s lessc")
   end
 
   def self.yui_available?
     !!defined?(YUI)
+  end
+
+  def self.load_configuration
+    @conf = YAML.load(File.read(config_path))
+    @dev_envs = @conf[:development_environments] || ['development']
+
+    @dev = defined?(Rails) ? (@dev_envs.include? Rails.env) : false
+  end
+
+  def self.dev?
+    @dev
+  end
+
+  def self.javascripts
+    @conf[:javascripts]
+  end
+
+  def self.stylesheets
+    @conf[:stylesheets]
+  end
+
+  def self.less(group)
+    self.stylesheets[group].select do |file|
+      /\.less$/ =~ file
+    end
+  end
+
+  def self.css(group)
+    self.stylesheets[group].select do |file|
+      /\.css$/ =~ file
+    end
   end
 end
--- lib/minify/helper.rb
+++ lib/minify/helper.rb
@@ -1,12 +1,61 @@
 module Minify
   module Helper
     def minify_stylesheets(*groups)
+      handle_css(*groups) + handle_less(*groups)
     end
 
     def minify_javascripts(*groups)
+      handle_js(*groups)
     end
 
     private
+
+    def less_file(file)
+      ActionView::Helpers::AssetTagHelper.stylesheet_link_tag file, :rel => 'stylesheet/less'
+    end
+
+    def css_file(file)
+      ActionView::Helpers::AssetTagHelper.stylesheet_link_tag file
+    end
+
+    def js_file(file)
+      ActionView::Helpers::AssetTagHelper.javascript_link_tag file
+    end
+
+    def less_js_file
+      if File.exists?(File.join(Minify::JAVASCRIPT_DIR, 'less.js'))
+        js_file 'less.js'
+      end
+    end
+
+    def handle_less(*groups)
+      if Minify.dev?
+        # Link to less files
+        (groups.map do |group|
+           Minify.less(group).map do |file|
+             less_file(file)
+           end
+         end + [less_js_file]).flatten.compact.join
+      end
+    end
+
+    def handle_css(*groups)
+      groups.each do |group|
+        if Minify.dev?
+        else
+          # Production
+          group_file = File.join('minify', "#{group}.css")
+
+          if File.exists?(File.join(Minify::STYLESHEET_DIR, group_file))
+            css_file group_file
+          else
+            Minify.css(group).map do |file|
+              css_file file
+            end.join
+          end
+        end
+      end
+    end
   end
 end
 
